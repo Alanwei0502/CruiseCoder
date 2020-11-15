adb209e0044e8fa04025c6e6172677adc5405987
Alanwei0502 <alans40609@gmail.com>
2020-11-16 00:52:18 +0800

    11/16 語宙試煉(切版)

--- frontEnd/galaxy.php
+++ frontEnd/galaxy.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>語宙試煉</title>
     <link rel="stylesheet" href="../css/galaxy.css">
+
 </head>
 
 <body>
@@ -20,19 +21,34 @@
         ?>
         <main>
 
-            <section class="galaxy">
+            <section class="universe">
                 <h2>&lt;數碼銀河&#47;&gt;</h2>
-                <div></div>
+                <div class="outGalaxy">
+                    <div class="inGalaxy"></div>
+                </div>
                 <article>
                     <h3>HTML星系簡介</h3>
-                    <P></P>
-                    <img src="" alt="外星人">
+                    <p>HTML是一種網頁使用的語言，主要用來管理網頁的架構，基本上現行的瀏覽器都可以讀取HTML，使用HTML可以編輯設計出網頁，也可以在網頁中加入所有HTML語言可支援的方式，例如表格、表單、圖片、文字、連結、程式等等。<br>一個好的網頁其 HTML 可以說是相當單純且具有易讀性，這種網頁不但方便前端工程師進行後續的維護外，也比較容易讓你的網頁增加曝光率讓搜尋引擎能把你的網頁擺在最前面，此種行為稱為 SEO，而目前 HTML 已經發布到第五版同時也是目前最廣為使用的版本，因此大家常聽到的 HTML5 其實就是指第五版的 HTML 喔！</p>
+                    <img src="../images/trial/monster/m_html.png" alt="外星人">
                 </article>
             </section>
 
-            <section class="planet">
-                <h2>星球關卡</h2>
-                <div class="carousel"></div>
+            <section class="planetQuiz">
+                <h2>&lt;星球關卡&#47;&gt;</h2>
+                <div class="carousel">
+                    <div id="planet1" class="planet">
+                        <img src="../images/trial/planets/html1.png" alt="初級html星球">
+                        <div>初級html星球</div>
+                    </div>
+                    <div id="planet2" class="planet">
+                        <img src="../images/trial/planets/html2.png" alt="中級html星球">
+                        <div>中級html星球</div>
+                    </div>
+                    <div id="planet3" class="planet">
+                        <img src="../images/trial/planets/html3.png" alt="高級html星球">
+                        <div>高級html星球</div>
+                    </div>
+                </div>
                 <article>
                     <h3>HTML初級星球</h3>
                     <p></p>
@@ -41,7 +57,7 @@
             </section>
 
             <section class="badge">
-                <h2>星系解鎖成就</h2>
+                <h2>&lt;星系解鎖成就&#47;&gt;</h2>
                 <p></p>
                 <div class="planetBadge">
                     <img src="" alt="HTML初級星球徽章">
@@ -57,6 +73,7 @@
         <?php
         include('layout/footer.php');
         ?>
+        <script src="../js/galaxy.js"></script>
     </div>
 </body>
 
