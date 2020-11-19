dff1dc916ce322b3b53b20e553d3f7b9fd481f0c
Alanwei0502 <alans40609@gmail.com>
2020-11-19 20:10:05 +0800

    11/19 隨意更新好爽喔

---
+++ frontEnd/quiz.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="zh-Hant">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cruise Coders | HTML初級星球</title>
+    <link rel="stylesheet" href="../css/main.css">
+
+</head>
+
+<body>
+    <!-- include('layout/login.php'); -->
+    <div class="wrap galaxy">
+        <?php
+        include('layout/spacebackground.php');
+        include('layout/header.php');
+        ?>
+        <main>
+        </main>
+        <?php
+        include('layout/footer.php');
+        ?>
+        <script src="../js/quiz.js"></script>
+    </div>
+</body>
+
+</html>
