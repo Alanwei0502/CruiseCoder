c3ee2f38bf0ec0700e8d8ec58b8bb092d79c33da
Alanwei0502 <alans40609@gmail.com>
2020-11-20 22:41:15 +0800

    no message

--- frontEnd/quiz.php
+++ frontEnd/quiz.php
@@ -22,7 +22,7 @@
         ?>
         <main>
             <h2>HTML初級星球</h2>
-            <section>
+            <section class="beforeQuiz">
                 <div class="notice">
                     <h3>測驗須知</h3>
                     <div>
@@ -36,6 +36,36 @@
                 </div>
                 <a href="#0">開始試煉</a>
             </section>
+
+            <!-- <section class="inQuiz">
+                <div class="notice">
+                    <h3>測驗須知</h3>
+                    <div>
+                        <p>測驗時間：2分鐘</p>
+                        <p>總共題目：3題</p>
+                        <p>確認選項後，請按下一題繼續作答</p>
+                    </div>
+                    <label for="checkOne"><input type="checkbox" id="checkOne">我不會諮詢任何外部來源（包括網站，書籍或人）或從中複製代碼來完成這些任務。</label>
+                    <label for="checkTwo"><input type="checkbox" id="checkTwo">我不會複製、分發或公開顯示我在此測試過程中遇到的任何信息。</label>
+
+                </div>
+                <a href="#0">開始試煉</a>
+            </section> -->
+
+            <!-- <section class="afterQuiz">
+                <div class="notice">
+                    <h3>測驗須知</h3>
+                    <div>
+                        <p>測驗時間：2分鐘</p>
+                        <p>總共題目：3題</p>
+                        <p>確認選項後，請按下一題繼續作答</p>
+                    </div>
+                    <label for="checkOne"><input type="checkbox" id="checkOne">我不會諮詢任何外部來源（包括網站，書籍或人）或從中複製代碼來完成這些任務。</label>
+                    <label for="checkTwo"><input type="checkbox" id="checkTwo">我不會複製、分發或公開顯示我在此測試過程中遇到的任何信息。</label>
+
+                </div>
+                <a href="#0">開始試煉</a>
+            </section> -->
         </main>
         <?php
         include('layout/footer.php');
