8bf8782339abe713aab431db9e5555c73cc0223f
Alanwei0502 <alans40609@gmail.com>
2020-11-20 22:26:06 +0800

    11/20 quiz更新

--- frontEnd/quiz.php
+++ frontEnd/quiz.php
@@ -23,7 +23,6 @@
         <main>
             <h2>HTML初級星球</h2>
             <section>
-                <img src="../images/quiz/spacebase.svg" alt="">
                 <div class="notice">
                     <h3>測驗須知</h3>
                     <div>
@@ -31,8 +30,8 @@
                         <p>總共題目：3題</p>
                         <p>確認選項後，請按下一題繼續作答</p>
                     </div>
-                    <label for=""><input type="checkbox">我不會諮詢任何外部來源（包括網站，書籍或人）或從中複製代碼來完成這些任務。</label>
-                    <label for=""><input type="checkbox">我不會複製、分發或公開顯示我在此測試過程中遇到的任何信息。</label>
+                    <label for="checkOne"><input type="checkbox" id="checkOne">我不會諮詢任何外部來源（包括網站，書籍或人）或從中複製代碼來完成這些任務。</label>
+                    <label for="checkTwo"><input type="checkbox" id="checkTwo">我不會複製、分發或公開顯示我在此測試過程中遇到的任何信息。</label>
 
                 </div>
                 <a href="#0">開始試煉</a>
