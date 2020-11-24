19df4a5cd14059bb85f15e7dc9878a9871ab72a1
solitary10907 <s147753369>
2020-11-24 17:32:38 +0800

    後台 article 燈箱修改 article js 新增 mixin input file樣式新增 後台圖片新增

--- backEnd/article.php
+++ backEnd/article.php
@@ -9,6 +9,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>後台 | 文章管理</title>
+  <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/themes/hot-sneaks/jquery-ui.css" rel="stylesheet">
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
   <link rel="stylesheet" href="./../css/main2.css">
 </head>
 
@@ -18,17 +21,16 @@
     include('layout/sideBar.php'); //aside
     ?>
 
-    <main>
-      <!-- 在這裡面codeing -->
+<main>
       <h2>專欄管理</h2>
       <div class="searchContent">
         <div class="upLoadTime">
           <label>上架日期</label>
-          <input type="text" placeholder="2020/11/27">
+          <input type="text" id="datepicker1" readonly="true">
         </div>
         <div class="status">
           <label>狀態</label>
-          <select name="country">
+          <select name="status">
             <option value="1" selected>上架</option>
             <option value="2">下架</option>
           </select>
@@ -47,58 +49,123 @@
       </div>
       <div class="table">
         <div class="tr">
-          <div class="td"><input type="checkbox"></div>
+          <div class="td"><label><input type="checkbox"><span></span></label></div>
           <div class="td">上架日期</div>
           <div class="td">狀態</div>
           <div class="td">專欄名稱</div>
           <div class="td">操作</div>
         </div>
         <div class="tr">
-          <div class="td"><input type="checkbox"></div>
+          <div class="td"><label><input type="checkbox"><span></span></label></div>
           <div class="td">2020/11/20</div>
           <div class="td">上架</div>
           <div class="td">十大好書推薦</div>
-          <div class="td"><button>編輯</button></div>
+          <div class="td"><button class="editBtn">編輯</button></div>
         </div>
         <div class="tr">
-          <div class="td"><input type="checkbox"></div>
+          <div class="td"><label><input type="checkbox"><span></span></label></div>
           <div class="td">2020/11/25</div>
           <div class="td">上架</div>
           <div class="td">自學力網站資源</div>
-          <div class="td"><button>編輯</button></div>
+          <div class="td"><button class="editBtn">編輯</button></div>
         </div>
-        <div class="tr">
-          <div class="td"><input type="checkbox"></div>
+        <div  class="tr">
+          <div class="td"><label><input type="checkbox"><span></span></label></div>
           <div class="td">2020/11/27</div>
           <div class="td">上架</div>
           <div class="td">推薦編譯器</div>
-          <div class="td"><button>編輯</button></div>
+          <div class="td"><button class="editBtn">編輯</button></div>
         </div>
         <div class="tr">
-          <div class="td"><input type="checkbox"></div>
+          <div class="td"><label><input type="checkbox"><span></span></label></div>
           <div class="td">2020/12/02</div>
           <div class="td">下架</div>
           <div class="td">軟體推薦資料</div>
-          <div class="td"><button>編輯</button></div>
+          <div class="td"><button class="editBtn">編輯</button></div>
         </div>
         <div class="tr">
-          <div class="td"><input type="checkbox"></div>
+          <div class="td"><label><input type="checkbox"><span></span></label></div>
           <div class="td">2020/12/03</div>
           <div class="td">上架</div>
           <div class="td">好用套件推薦</div>
-          <div class="td"><button>編輯</button></div>
+          <div class="td"><button class="editBtn">編輯</button></div>
         </div>
       </div>
 
-      <ul>
-        <li><img src="./../images/backEnd/pagearrow.png" alt="無法顯示圖片"></li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li class="lastLi"><img src="./../images/backEnd/pagearrow.png" alt="無法顯示圖片"></li>
-      </ul>
-    </main><!-- 在這裡面codeing -->
+      <div class="addArticleBackAll">
+        <div class="addArticleBack"></div>
+        <div class="addArticle">
+          <img src="./../images/backEnd/blackCancel.png" alt="無法顯示圖片" class="cancelBack">
+          <h2>新增專欄</h2>
+          <form action="" class="addArticleForm">
+            <div class="articleTop">
+              <div class="articleName">
+                <label>專欄名稱</label>
+                <input type="text">
+                <label>專欄預覽圖</label>
+                <div class="fileStyle">
+                  <input type="file" class="inputFile">
+                  <p class="pFileName"></p>
+                  <button></button>
+                </div>
+              </div>
+              <div class="articleImageOutline">
+                <div class="articleImage"></div>
+              </div>
+            </div>
+            <div class="articleBottom">
+              <label>專欄內容</label>
+              <textarea name="" id=""></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="editArticleBackAll">
+        <div class="editArticleBack"></div>
+        <div class="editArticle">
+          <img src="./../images/backEnd/blackCancel.png" alt="無法顯示圖片" class="cancelBack">
+          <h2>專欄資訊</h2>
+          <form action="" class="editArticleForm">
+            <div class="articleTop">
+              <div class="articleName">
+                <p class="articleNumber">專欄編號: A0003</p>
+                <label>專欄名稱</label>
+                <input type="text">
+                <label>專欄預覽圖</label>
+                <div class="fileStyle">
+                  <input type="file" class="inputFile">
+                  <p class="pFileName"></p>
+                  <button></button>
+                </div>
+                <label>狀態</label>
+                <select>
+                  <option value="1" selected>上架</option>
+                  <option value="2">下架</option>
+                </select>
+              </div>
+              <div class="articleImageOutline">
+                <div class="articleImage"></div>
+              </div>
+            </div>
+            <div class="articleBottom">
+              <label>專欄內容</label>
+              <textarea name="" id=""></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      
+
+      <div class="changePage">
+        <button class="lastPage">上一頁</button>
+        <button class="nextPage">下一頁</button>
+      </div>
+    </main>
   </div>
+  <script src="./../js/datepicker.js"></script>
+  <script src="./../js/article.js"></script>
 </body>
 
 </html>
