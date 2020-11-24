dc23def8f14d7ae780619c447ecaf3eb47e72d7d
Alanwei0502 <alans40609@gmail.com>
2020-11-25 00:03:28 +0800

    R~~~~~~完了啦!

--- js/galaxy.js
+++ js/galaxy.js
@@ -8,44 +8,35 @@ var planetThree = document.getElementsByClassName('planetThree')[0];
 
 for (let i = 0; i < planet.length; i++) {
     planet[i].addEventListener("click", function () {
-        if (i === 0) {
+        if (i === 0) {//點擊藍色那顆
             planetOne.classList.add('planetTwo');
-            planetOne.classList.remove('planetThree');
-            planetOne.classList.remove('planetOne');
+            planetOne.classList.remove('planetThree', 'planetOne');
 
             planetTwo.classList.add('planetThree');
-            planetTwo.classList.remove('planetTwo');
-            planetTwo.classList.remove('planetOne');
+            planetTwo.classList.remove('planetTwo', 'planetOne');
 
             planetThree.classList.add('planetOne');
-            planetThree.classList.remove('planetThree');
-            planetThree.classList.remove('planetTwo');
+            planetThree.classList.remove('planetThree', 'planetTwo');
         }
-        if (i === 1) {
+        if (i === 1) {//點擊紅色那顆
             planetOne.classList.add('planetOne');
-            planetOne.classList.remove('planetThree');
-            planetOne.classList.remove('planetTwo');
+            planetOne.classList.remove('planetThree', 'planetTwo');
 
             planetTwo.classList.add('planetTwo');
-            planetTwo.classList.remove('planetThree');
-            planetTwo.classList.remove('planetOne');
+            planetTwo.classList.remove('planetThree', 'planetOne');
 
             planetThree.classList.add('planetThree');
-            planetThree.classList.remove('planetOne');
-            planetThree.classList.remove('planetTwo');
+            planetThree.classList.remove('planetOne', 'planetTwo');
         }
-        if (i === 2) {
+        if (i === 2) {//點擊綠色那顆
             planetOne.classList.add('planetThree');
-            planetOne.classList.remove('planetTwo');
-            planetOne.classList.remove('planetOne');
+            planetOne.classList.remove('planetTwo', 'planetOne');
 
             planetTwo.classList.add('planetOne');
-            planetTwo.classList.remove('planetTwo');
-            planetTwo.classList.remove('planetThree');
+            planetTwo.classList.remove('planetTwo', 'planetThree');
 
             planetThree.classList.add('planetTwo');
-            planetThree.classList.remove('planetThree');
-            planetThree.classList.remove('planetOne');
+            planetThree.classList.remove('planetThree', 'planetOne');
         }
     });
 }
