9d85f3a681aec5fc5b15673299a59ba90f9d5c29
Alanwei0502 <alans40609@gmail.com>
2020-11-25 21:57:18 +0800

    11/25 header修改

--- frontEnd/layout/header.php
+++ frontEnd/layout/header.php
@@ -3,65 +3,77 @@
 ?>
 
 <header>
-      <div class="headerLeft">
-        <a href="./index.php">
-          <img class="logo" src="./../images/logoCruise.png" alt="圖片無法顯示">
-        </a>
-      </div>
-      <div class="headerMiddle">
-        <nav>
-          <ul>
-            <li>
-              <a href="./allCourse.php">外星課程</a>
+  <div class="headerLeft">
+    <a href="./index.php">
+      <img class="logo" src="./../images/logoCruise.png" alt="圖片無法顯示">
+    </a>
+  </div>
+  <div class="headerMiddle">
+    <nav>
+      <ul>
+        <li>
+          <a href="./allCourse.php">外星課程</a>
+        </li>
+        <li>
+          <a href="./galaxy.php">語宙試煉</a>
+        </li>
+        <li>
+          <a href="./article.php">太空補給站</a>
+        </li>
+        <li>
+          <a href="./tutorial.php">蟲洞練功坊</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+  <div class="headerRight">
+    <ul>
+      <li class="shoppingCar">
+        <label><input type="checkbox"><span></span></label>
+        <!-- <img src="./../images/shoppingIcon.png" alt="圖片無法顯示"> -->
+        <section>
+          <ul class="shoppingFancybox">
+            <li class="shopping">
+              <div class="image"></div>
+              <div class="info">
+                <p class="courseTitle">一變應萬變：RWD 響應式網頁設計</p>
+                <div class="itemTextButtom">
+                  <p class="status">已開課</p>
+                  <p class="price">NT$ 1,200</p>
+                </div>
+              </div>
             </li>
-            <li>
-              <a href="./galaxy.php">語宙試煉</a>
-            </li>
-            <li>
-              <a href="./article.php">太空補給站</a>
-            </li>
-            <li>
-              <a href="./tutorial.php">蟲洞練功坊</a>
+            <li class="shopping">
+              <div class="image"></div>
+              <div class="info">
+                <p class="courseTitle">張老闆帶你飛</p>
+                <div class="itemTextButtom">
+                  <p class="status">已開課</p>
+                  <p class="price">NT$ 1,200</p>
+                </div>
+              </div>
             </li>
           </ul>
-        </nav>
-      </div>
-      <div class="headerRight">
-        <img src="./../images/backEnd/memberIcon.png" id="member" alt="圖片無法顯示">
-        <div class="memberNav">
-          <ul>
-            <li><a href="./ccPoint.php">CC Point : 200</a></li>
-            <li><a href="./info.php">個人檔案</a></li>
-            <li><a href="./order.php">訂單資訊</a></li>
-            <li><a href="#">登出</a></li>
-          </ul>
-        </div>
-        <img src="./../images/shoppingIcon.png" alt="圖片無法顯示">
-        <div class="shoppingFancybox">
-          <div class="shopping">
-            <div class="itemImage"><img src="./../images/article/panel.png" alt="無法顯示圖片"></div>
-            <div class="itemText">
-              <p class="courseTitle">職人必修的 RWD 網頁入門</p>
-              <div class="itemTextButtom">
-                <p class="status">已開課</p>
-                <p class="price">NT$ 1,200</p>
-              </div>
-            </div>
-          </div>
-          <div class="shopping">
-            <div class="itemImage"><img src="./../images/article/panel.png" alt="無法顯示圖片"></div>
-            <div class="itemText">
-              <p class="courseTitle">職人必修的 RWD 網頁入門</p>
-              <div class="itemTextButtom">
-                <p class="status">已開課</p>
-                <p class="price">NT$ 1,200</p>
-              </div>
-            </div>
-          </div>
           <div class="shoppingTotal">
             <p>總計 2 堂課<br><span>NT$ 2,400</span></p>
-            <button>前往購物車</button>
+            <a href="./checkOut.php">前往購物車</a>
           </div>
-        </div>
-      </div>
-    </header>
+        </section>
+      </li>
+      <li class="member">
+        <label><input type="checkbox"><span></span></label>
+        <!-- <img src="./../images/backEnd/memberIcon.png" alt="圖片無法顯示"> -->
+        <ul>
+          <li><a href="./ccPoint.php" class="ccp">200</a></li>
+          <li class="smlSize"><a href="./allCourse.php">外星課程</a></li>
+          <li class="smlSize"><a href="./galaxy.php">語宙試煉</a></li>
+          <li class="smlSize"><a href="./article.php">太空補給站</a></li>
+          <li class="smlSize"><a href="./tutorial.php">蟲洞練功坊</a></li>
+          <li><a href="./info.php">個人檔案</a></li>
+          <li><a href="./order.php">訂單資訊</a></li>
+          <li><a href="#">登出</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</header>
