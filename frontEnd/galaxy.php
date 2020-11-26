0d2c8d8855991c34029f95b8bc3efed4e5fb6695
Alanwei0502 <alans40609@gmail.com>
2020-11-26 21:40:31 +0800

    11/26 09:40

--- frontEnd/galaxy.php
+++ frontEnd/galaxy.php
@@ -21,10 +21,20 @@
         include('layout/header.php');
         ?>
         <main>
+            <section class="introduction">
+                <h2>&lt; 語宙試煉 &#47;&gt;</h2>
+                <p>挑戰者你好，歡迎來到語宙試煉！每個星系都會有三個星球關卡，等級分別是初級、中級、高級，成功挑戰後，即可獲得星球徽章，快來展現你的實力吧！</p>
+                <img src="../images/trial/monster/m_html.png" alt="外星人">
+            </section>
 
             <section class="universe">
                 <h2>&lt; 數碼銀河 &#47;&gt;</h2>
                 <div class="outGalaxy">
+                    <div class="scrolldown">
+                        <p>點擊星系後，向下滑動以查看星球關卡。</p>
+                        <img class="arrow1" src="../images/trial/arrow.png" alt="">
+                        <img class="arrow2" src="../images/trial/arrow.png" alt="">
+                    </div>
                     <div class="inGalaxy">
                         <ul>
                             <li>
@@ -87,11 +97,6 @@
                         </ul>
                     </div>
                 </div>
-                <article>
-                    <h3>HTML星系簡介</h3>
-                    <p>HTML是一種網頁使用的語言，主要用來管理網頁的架構，基本上現行的瀏覽器都可以讀取HTML，使用HTML可以編輯設計出網頁，也可以在網頁中加入所有HTML語言可支援的方式，例如表格、表單、圖片、文字、連結、程式等等。<br>一個好的網頁其 HTML 可以說是相當單純且具有易讀性，這種網頁不但方便前端工程師進行後續的維護外，也比較容易讓你的網頁增加曝光率讓搜尋引擎能把你的網頁擺在最前面，此種行為稱為 SEO，而目前 HTML 已經發布到第五版同時也是目前最廣為使用的版本，因此大家常聽到的 HTML5 其實就是指第五版的 HTML 喔！</p>
-                    <img src="../images/trial/monster/m_html.png" alt="外星人">
-                </article>
             </section>
 
             <section class="planetQuiz">
@@ -113,6 +118,7 @@
                 <article>
                     <h3>HTML初級星球</h3>
                     <p>如果你是剛開始學習HTML，可是試試看先挑戰這顆HTML初級星球關卡，達到標準後，可以獲得此顆星球認證喔！</p>
+                    <img src="../images/trial/badge/html1.png" alt="HTML初級星球徽章">
                     <a href="quiz.php" class="goToQuiz">進入試煉</a>
                 </article>
             </section>
@@ -120,11 +126,6 @@
             <section class="badge">
                 <h3>星系解鎖成就</h3>
                 <p>當你成功挑戰完所有HTML的星球關卡，即可獲得HTML星系徽章！</p>
-                <div class="planetBadge">
-                    <img src="../images/trial/badge/html1.png" alt="HTML初級星球徽章">
-                    <img src="../images/trial/badge/html2.png" alt="HTML中級星球徽章">
-                    <img src="../images/trial/badge/html3.png" alt="HTML高級星球徽章">
-                </div>
                 <div class="galaxyBadge">
                     <img src="../images/trial/badge/html.png" alt="HTML星系徽章">
                 </div>
@@ -134,9 +135,9 @@
         <?php
         include('layout/footer.php');
         ?>
-        <script src="../js/galaxy.js"></script>
-        <script src="../js/header.js"></script>
     </div>
+    <script src="../js/galaxy.js"></script>
+    <script src="../js/header.js"></script>
 </body>
 
 </html>
