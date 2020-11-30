359fcb169ae30abbf83384894ed124082313964e
solitary10907 <s147753369>
2020-12-01 02:58:26 +0800

    article 模糊搜尋功能(感覺可以再更順一點@@?)

--- js/article.js
+++ js/article.js
@@ -10,3 +10,29 @@ prePage.addEventListener("click", function(){
   articleContent.classList.add("none");
   articleTitle.classList.remove("none");
 });
+
+
+
+$(document).ready(function(){
+  // 模糊搜尋功能
+  $("input.search").keyup(function(e){
+    let articleName = $(this).val();
+    let articleInside = $(".articleInside");
+    let articleh4 = articleInside.children("h4");
+
+
+    if(articleName!=""){
+      for(i = 0; i < articleInside.length; i++){
+        if(articleh4[i].innerText.includes(articleName)){
+          articleh4[i].closest("div.articleInside").classList.remove("none");
+        }else{
+          articleh4[i].closest("div.articleInside").classList.add("none")
+        }
+      }
+    }else{
+      for(i = 0; i < articleInside.length; i++){
+        articleh4[i].closest("div.articleInside").classList.remove("none");   
+      }
+    }
+  });
+});
