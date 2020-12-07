23a16705063b43814b4a38a19cc6c93077192f81
Alanwei0502 <alans40609@gmail.com>
2020-12-07 14:48:30 +0800

    connect database updating

--- frontEnd/layout/connect.php
+++ frontEnd/layout/connect.php
@@ -1,9 +1,14 @@
 <?php
-// host在大家電腦可能要改名字
+// 家偉專用
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "soyalan0502";
 $db_select = "cruisecoder";
+// 其他人請用
+// $db_host = "10.2.0.188";
+// $db_user = "root";
+// $db_pass = "1qaz2wsx";
+// $db_select = "cruisecoder";
 
 $dsn = "mysql:host=" . $db_host . ";dbname=" . $db_select;
 
